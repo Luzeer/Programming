07d98509f5d253a8cb65827b9a3774eb8e713d1a
Luzeer <31651138+Luzeer@users.noreply.github.com>
2017-12-01 02:30:55 +0300

    Delete сосатель.py

--- hw_5/сосатель.py
+++
@@ -1,9 +0,0 @@
-n = input('введите свое имя: ')
-with open('text.txt', 'w', encoding='utf-8') as f:
-    a=input('введите свое послание Лиле: ')
-    while a !='':
-        a=input('введите свое послание Лиле: ')
-        #print(a)
-        if a[-3:]=='хуй':
-            print('сама соси,', n)
-            break
