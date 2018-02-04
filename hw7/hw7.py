586d7ee72ce6d130f0413eff3f4bea23d3a17e37
Luzeer <31651138+Luzeer@users.noreply.github.com>
2018-02-05 01:27:00 +0300

    Create hw7.py

---
+++ hw7/hw7.py
@@ -0,0 +1,38 @@
+#var5
+import collections
+
+def file(name):
+    with open(name, encoding='utf-8') as f:
+        text = f.read()
+    for i in ['.',',','?','!',';',':','"','- ','-','—','(',')',"'",'”','’']: 
+        text = text.replace(i,'')
+    text = text.lower()
+    words = text.split()
+    return words
+
+def adj_ous(name):
+    adj_ous = []
+    for word in file(name):
+        if word.endswith('ous'):
+            adj_ous.append(word)
+    return adj_ous
+
+def length(name):
+    length = {}
+    for word in adj_ous(name):
+    	length[word] = len(word)
+    return length
+
+def middle_len(name):
+    s=0
+    for value in length(name).values():
+        s+=value
+    ml=s/len(length(name))
+    return ml
+
+def adj_print(name):
+    print ('В тексте ', len(length(name)), ' существительных с суффиксом -ous-.')
+    print('Их средняя длина: ', middle_len(name))
+
+
+adj_print(input('Введите название файла: '))
