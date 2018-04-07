912263737baabfe0442440c93331ba944dfaf6f7
Luzeer <31651138+Luzeer@users.noreply.github.com>
2018-04-07 12:07:03 +0300

    Update ex2.py

--- kr-v2-07-04-18/ex1/ex2.py
+++ kr-v2-07-04-18/ex1/ex2.py
@@ -1,26 +1,29 @@
 import re
+import collections
 
 def rfile():
     with open('mystem.xml', 'r', encoding='utf-8') as f:
-        return f.read().split('\n')
+        return f.read()
 
 def wfile(text):
     with open('out.txt', 'w', encoding='utf-8') as f:
         f.write(str(text))
     if not f.writable:
         print('Файл недоступен')
-
-def diction(text):
-    dict = {}
-    for line in text:
-        if re.search("[а-я]+", line, flags=re.IGNORECASE):
-            j = 0
-            l = re.search("gr=.+",line)
-            for line1 in text:
-                if line == line1:
-                    j += 1
-            dict[l] = j
-    dict = sorted(dict.items(), key=lambda value: value[1])
+        
+def diction(text):        
+    forms=re.findall(r'gr="(.*)"', text)
+    dict={}
+    for a in forms:
+        if a in dict:
+            val=dict[a]
+            dict[a]=val+1
+        else:
+            dict[a]=1
+    dict=sorted(dict)
+    
+    for key in dict:
+        print(key)
     return (dict)
 
 wfile(diction(rfile()))
